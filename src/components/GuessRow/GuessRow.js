69c4832d7817bb74ff4f932dc243649ea291ddb3
Victor Caruso <vic.caruso@gmail.com>
2023-07-06 11:34:09 -0700

    Create GuessRow component and set up basic scaffolding

---
+++ src/components/GuessRow/GuessRow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function GuessRow() {
+  return (
+    <>
+      <p className="guess">
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+      </p>
+      <p className="guess">
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+      </p>
+    </>
+  );
+}
+
+export default GuessRow;
