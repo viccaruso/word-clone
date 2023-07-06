1886f212e8a614130ef036daeec728dd0db7e0ef
Victor Caruso <vic.caruso@gmail.com>
2023-07-06 11:09:41 -0700

    Move components out of Game component directory

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import GuessInput from './GuessInput/GuessInput';
+import GuessInput from '../GuessInput/GuessInput';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import GuessTracker from './GuessTracker/GuessTracker';
+import GuessTracker from '../GuessTracker/GuessTracker';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
