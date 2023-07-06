984734c17a52cbec8fda15c95e89e1633b638c0f
Victor Caruso <vic.caruso@gmail.com>
2023-07-06 12:19:43 -0700

    Prevent user from submitting too many guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import GuessInput from '../GuessInput/GuessInput';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessTracker from '../GuessTracker/GuessTracker';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -14,7 +15,9 @@ function Game() {
   const [guesses, setGuesses] = React.useState(['HELLO', 'THERE', 'WORLD']);
 
   function handleGuess(guess) {
+    if (guesses.length >= NUM_OF_GUESSES_ALLOWED) return;
     const nextGuesses = [...guesses, guess];
+    console.log(nextGuesses);
     setGuesses(nextGuesses);
   }
   return (
