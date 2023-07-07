449584b4b786f4837e546d17c25b7cf299f676f4
Victor Caruso <vic.caruso@gmail.com>
2023-07-07 13:32:06 -0700

    Remove console logs

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,8 +9,6 @@ import GameResultBanner from '../GameResultBanner/GameResultBanner';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
@@ -30,7 +28,6 @@ function Game() {
     }
 
     const result = checkGuess(guess, answer);
-    console.log(result);
     setGuesses([...guesses, result]);
   }
 
